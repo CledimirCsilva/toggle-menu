48d38b3c2a32fc562376696663adc0100310b459
Cledimir C Silva <csilva@ccsweb.com.br>
2020-10-28 23:40:48 -0300

    Animated logo inserted in menu

--- js/toggle-menu.js
+++ js/toggle-menu.js
@@ -14,6 +14,7 @@ const toggleMenuHandler = () => {
     const btnBar2_class = getClass("#btnBar2", "none", "btnBar2-rotate");
     const btnBar3_class = getClass("#btnBar3", "none", "btnBar3-hide");
     const navbarClass   = getClass("#navbar", "none", "navbarHeight");
+    const logotype      = getClass("#logotype", "none", "logoChangePosition");
 
     $(window).scroll( () => {
         const scroll = $(window).scrollTop();
@@ -24,13 +25,13 @@ const toggleMenuHandler = () => {
             setClass("#btnBar2");
             setClass("#btnBar3");
         }
-        
     });
 
     setClass("#btnBar1", "class", btnBar1_class);        
     setClass("#btnBar2", "class", btnBar2_class);        
     setClass("#btnBar3", "class", btnBar3_class);        
     setClass("#navbar", "class", navbarClass);  
+    setClass("#logotype", "class", logotype);
     
 }
 
